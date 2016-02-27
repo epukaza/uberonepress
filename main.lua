4e5e10e455dc0079fc799ac41e4d267d8051a4a3
Abhinav Sinha <abhinav.s.sinha@gmail.com>
2016-02-27 01:02:10 -0800

    added authorization tokens

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
 print("this is main.lua")
+
+
+sk = net.createConnection(net.TCP, 1)
+sk:on("receive", function(sck, c) print(c) end )
+sk:connect(443,"www.reddit.com")
+sk:on("connection", function(sck,c)
+    -- Wait for connection before sending.
+    sk:send("GET /index.html HTTP/1.1\r\nHost: www.reddit.com\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
+end)
